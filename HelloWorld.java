e1ef2a848d5e1a008efda7493243e902c283f435
aryantisiregar <aryantisiregar2010@gmail.com>
2022-08-23 22:23:20 +0700

    add print

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+        System.out.println(x: "Hello, Yan!");
+        System.out.println(x: "Hello, Whatsupp!"); 
     }
 }
